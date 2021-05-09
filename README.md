61eb3c9aec4eb0e5fcc5774a4cc0b5bcaade07e2
Rusty Pedrosa <rusty@pedrosatech.com>
2021-05-08 23:54:27 -0700

    WIP: Test queries

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 ## connection:
 mongodb+srv://dbUser:dbUserPassword@cluster0.zsakm.mongodb.net/movie_ratings?retryWrites=true&w=majority
+
+## Import imdb data
+### To sharded server:
+```
+mongoimport --uri "mongodb+srv://dbUser:dbUserPassword@cluster0.zsakm.mongodb.net/movie_ratings?retryWrites=true&w=majority" -c ratings --type tsv --file title.ratings.t
+sv --headerline
+```
+...
+
+### Local test instance
+```
+mongoimport --authenticationDatabase admin --username admin --password cmpe172 -d movie -c title --type tsv --file title.tsv --headerline
+mongoimport --authenticationDatabase admin --username admin --password cmpe172 -d movie -c ratings --type tsv --file title.ratings.tsv --headerline
+mongoimport --authenticationDatabase admin --username admin --password cmpe172 -d movie -c principals --type tsv --file principals.tsv --headerline
+mongoimport --authenticationDatabase admin --username admin --password cmpe172 -d movie -c crew --type tsv --file crew.tsv --headerline
+```
+
--- movie/src/main/java/edu/sjsu/movie/MovieApplication.java
+++ movie/src/main/java/edu/sjsu/movie/MovieApplication.java
@@ -7,6 +7,9 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.ServerAddress;
 
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
 import com.mongodb.client.MongoCollection;
 
 import org.bson.Document;
@@ -30,8 +33,17 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
 public class MovieApplication implements CommandLineRunner {
 
+    // private static final String connectionString = "mongodb+srv://dbUser:dbUserPassword@cluster0.zsakm.mongodb.net/movie_ratings?retryWrites=true&w=majority";
     private static final String connectionString = "mongodb://admin:cmpe172@localhost:27017";
     private static final String databaseName = "movie";
+
+    MongoClient mongoClient;
+    MongoDatabase database;
+
+    MongoCollection<Document> title;
+    MongoCollection<Document> ratings;
+    MongoCollection<Document> principals;
+    MongoCollection<Document> crew;
 
     public static void main(String[] args) {
         SpringApplication.run(MovieApplication.class, args);
@@ -41,22 +53,44 @@ public class MovieApplication implements CommandLineRunner {
     public void run(String... args) {
         log.info("Application started!");
         log.info("Connecting to database...");
-        MongoClient mongoClient = MongoClients.create(connectionString);
+        mongoClient = MongoClients.create(connectionString);
         log.info("MongoClient created: " + mongoClient.toString());
 
         log.info("Accessing database...");
-        MongoDatabase database = mongoClient.getDatabase(databaseName);
+        database = mongoClient.getDatabase(databaseName);
         log.info("MongoDatabase created: " + database.toString());
 
+        testQuery();
+
+        log.info("Loading collections...");
+        title = database.getCollection("title");
+        ratings = database.getCollection("ratings");
+        principals = database.getCollection("principals");  // Actors, etc
+        crew = database.getCollection("crew"); // Directors, writers
+        log.info("Collections loaded.");
+
+        // aggregation test
+        genreAverageYear("Short");
+
+    }
+    
+    public void testQuery() {
         log.info("Getting ratings collection...");
         MongoCollection<Document> ratingsCollection = database.getCollection("ratings");
         log.info("MongoCollection ratingsCollection created: " + ratingsCollection.toString());
 
         Document myDoc = ratingsCollection.find().first();
-        System.out.println("First item in ratings collection:");
+        System.out.println("Test query: First item in ratings collection:");
         System.out.println(myDoc.toJson());
+    }
 
-        // System.out.println("1. Movies with the highest ratings");
-
+    public void genreAverageYear(String genre) {
+        title.aggregate(
+            Arrays.asList(
+                Aggregates.match(Filters.eq("genres", "Short")),
+                Aggregates.group("$genres", Accumulators.average("startYear")  // .sum("count", 1))
+                )
+            ).forEach(doc -> System.out.println(doc.toJson())
+        );
     }
 }
